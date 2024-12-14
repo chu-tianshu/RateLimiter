8ce041b17a8a6c22ec3680e57f859e90a307631f
unknown <cts2003258@gmail.com>
2024-12-14 00:23:50 -0800

    Token bucket

---
+++ RateLimiter/RateLimiter/Middlewares/TokenBucketRateLimiterMiddleware.cs
@@ -0,0 +1,30 @@
+﻿namespace RateLimiter.Middlewares
+{
+    using System.Net;
+    using RateLimiter.Services;
+
+    public class TokenBucketRateLimiterMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public TokenBucketRateLimiterMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (BucketManagementService.RemainingTokenCount > 0)
+            {
+                BucketManagementService.ConsumeOneToken();
+                await this.next(context);
+            }
+            else
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.TooManyRequests;
+
+                return;
+            }
+        }
+    }
+}
---
+++ RateLimiter/RateLimiter/Services/BucketManagementService.cs
@@ -0,0 +1,39 @@
+﻿namespace RateLimiter.Services
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class BucketManagementService : BackgroundService
+    {
+        private const int BucketSize = 5;
+        private const int TokensRefilledPerSecond = 1;
+        private static readonly TimeSpan Interval = TimeSpan.FromSeconds(1);
+
+        public static int RemainingTokenCount { get; private set; } = 0;
+
+        public static void ConsumeOneToken()
+        {
+            if (RemainingTokenCount == 0)
+            {
+                return;
+            }
+
+            RemainingTokenCount--;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            using (PeriodicTimer timer = new PeriodicTimer(Interval))
+            {
+                while (!stoppingToken.IsCancellationRequested && await timer.WaitForNextTickAsync(stoppingToken))
+                {
+                    if (RemainingTokenCount < BucketSize)
+                    {
+                        RemainingTokenCount += TokensRefilledPerSecond;
+                    }
+                }
+            }
+        }
+    }
+}
